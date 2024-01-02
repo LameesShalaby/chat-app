cab75581b65b77f33925a14e76052f13fef0f64a
LameesShalaby <lameesshalaby254@gmail.com>
2024-01-02 15:30:32 +0200

    chat-app

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/chat-app/',
     plugins: [react()],
     build: {
         chunkSizeWarningLimit: 3000, // Set a limit that suits your needs
     },
+
 })
